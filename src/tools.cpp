b22ae403d9f687c9adb5aabe0ca03fe692fb4e51
mstrens <mstrens@yahoo.fr>
2023-02-28 10:21:12 +0100

    bug fix rounding

--- src/tools.cpp
+++ src/tools.cpp
@@ -59,7 +59,7 @@ bool __no_inline_not_in_flash_func(get_bootsel_button)() {
 
 int32_t int_round(int32_t n, uint32_t d)
 {
-    int32_t offset = ((n < 0) ? d : -d) / 2;
+    int32_t offset = ((n > 0) ? d : -d) / 2;
     return (n + offset) / d;
 }
 
