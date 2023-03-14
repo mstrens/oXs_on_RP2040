a2f4e6dcb205c78ba8ad37fd5166f7f4de955c24
mstrens <mstrens@yahoo.fr>
2023-03-14 19:10:40 +0100

    more debugging sdp3x

--- src/sdp3x.cpp
+++ src/sdp3x.cpp
@@ -34,7 +34,13 @@ void SDP3X::begin() {
   temperatureKelvin = 273.15 + temperatureCelsius ; // in Kelvin
   dpScaleSdp3x =  966.0 / 1013.0 / 60 ; //60=SDP31 , 240=SDP32
 #else // not a simulation
+    
+    uint8_t readBuffer[9];
+    int32_t i2cError;
+    
     printf("trying to read sdpXX sensor with i2c adress = %X\n", _address);
+    i2cError =  i2c_read_timeout_us (i2c1 , _address , &readBuffer[0] , 3 , true, 5500) ;    
+    printf("read 3 bytes before the first write; return code  = %d \n", (int) i2cError);
     
     // set the sensor in continous mode with averaging (send a command 0X3615)
     uint8_t cmdData[2] = { 0X36 , 0X15} ; 
@@ -43,8 +49,6 @@ void SDP3X::begin() {
     return ; 
     sleep_ms(20); // wait 20msec in order to get the first data (datasheet says 8 msec) 
     }
-    uint8_t readBuffer[9];
-    int32_t i2cError;
     //if ( i2c_read_timeout_us (i2c1 , _address , &readBuffer[0] , 9 , false, 5500) < 0)  {
     /*
     i2cError = i2c_read_timeout_us (i2c1 , _address , &readBuffer[0] , 1 , true, 5500);   
