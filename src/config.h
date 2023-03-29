fb67088503763c423d0b87997bad50618eee7a74
mstrens <mstrens@yahoo.fr>
2023-03-29 12:50:25 +0200

    2ms in gpi ini

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <stdint.h>
-#define VERSION "2.0.1"
+#define VERSION "2.0.2"
 
 #define DEBUG  // force the MCU to wait for some time for the USB connection; still continue if not connected
 
