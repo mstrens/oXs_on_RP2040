d73ea15014223ad39fb52945522e04598f9dd0d1
mstrens <mstrens@yahoo.fr>
2023-07-01 10:59:55 +0200

    change version nr

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <stdint.h>
-#define VERSION "2.6.0"
+#define VERSION "2.6.1"
 
 //#define DEBUG  // force the MCU to wait for some time for the USB connection; still continue if not connected
 
