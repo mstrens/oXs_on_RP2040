dd91f7e87b25d7b770da8154f0156a006d24a27e
mstrens <mstrens@yahoo.fr>
2023-10-15 17:14:44 +0200

    2.8.32 upload again on github

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <stdint.h>
-#define VERSION "2.8.31" 
+#define VERSION "2.8.32" 
 
 //#define DEBUG  // force the MCU to wait for some time for the USB connection; still continue if not connected
 
