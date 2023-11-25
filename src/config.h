8cef251abe3b3e171e2ff43a5582d7147b9d701e
mstrens <mstrens@yahoo.fr>
2023-11-25 09:30:18 +0100

    2.10.21 add srxl2 msg

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <stdint.h>
-#define VERSION "2.10.20"
+#define VERSION "2.10.21"
 
 //#define DEBUG  // force the MCU to wait for some time for the USB connection; still continue if not connected
 
